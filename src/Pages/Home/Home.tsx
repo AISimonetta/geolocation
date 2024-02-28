d13f2045e27d942797142a4f583050a57ef61453
AISimonetta <info@simonettaalejandra.com>
2024-02-28 11:10:20 +0000

    gitignore updated, buttins added to home page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,9 +4,9 @@ import './Home.scss'
 const Home = () => {
   return (
     <div className='home home__linear-gradient'>
-     <Link to='../UserLocation/UserLocation.tsx'><button>My Location</button></Link>
-     <Link to='../Weather/Weather.tsx'><button>Today's weather</button></Link>
-     <Link to='../ToDoList/ToDoList.scss'><button>Today's to do list</button></Link>
+     <Link to='../UserLocation/UserLocation.tsx'><button className='home__locationBtn'>My Location</button></Link>
+     <Link to='../Weather/Weather.tsx'><button className='home__weatherBtn'>Today's weather</button></Link>
+     <Link to='../ToDoList/ToDoList.scss'><button className='home__toDoListBtn'>Today's to do list</button></Link>
       
     </div>
   )
