5a84d30d11a28aeb0b2a69866f7c88b332dfd90b
AISimonetta <info@simonettaalejandra.com>
2024-03-04 13:57:34 +0000

    mobile view for home & weather finished

--- src/Container/WeatherCard/WeatherCard.tsx
+++ src/Container/WeatherCard/WeatherCard.tsx
@@ -17,16 +17,16 @@ const WeatherCard = ({ temp_c, condition, wind_mph }: WeatherCardProps) => {
         </h1>
         <ul className="weather__list">
           <li className="weather__list--item">
-            <ArrowRightCircle className="weather__icon" />
+            <ArrowRightCircle className="weather__icon--list" />
             Temperature in celsius: <span>{temp_c} Celsius </span>
           </li>
           <li className="weather__list--item">
-            <ArrowRightCircle className="weather__icon" />
+            <ArrowRightCircle  className="weather__icon--list" />
             Weather condition: <span>{condition.text}</span>
             <img src={condition.icon} alt="weather icon" />
           </li>
           <li className="weather__list--item">
-            <ArrowRightCircle className="weather__icon" />
+            <ArrowRightCircle className="weather__icon--list" />
             Wind speed in miles per hour: <span>{wind_mph}</span>
           </li>
         </ul>
